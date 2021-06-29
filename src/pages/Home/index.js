72153174dddceb7f952600a12783090f9c0c4c69
Edinaldo <edinaldolemos@yahoo.com.br>
2021-06-28 22:23:35 -0300

    create footer

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import Header from './components/Header'
+import Header from './components/Header';
+import Footer from './components/footer';
 import './style.css';
 function Home(){
     return (
@@ -13,6 +14,7 @@ function Home(){
              feed
              </div>
          </main>
+         <Footer/>
      </div>
     );
 }
